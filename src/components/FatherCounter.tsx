410f6245474ccbb4cbccd54b3f6df47724b77be3
ddfilipov <denisfilipov12+3@gmail.com>
2022-11-20 14:42:26 +0100

    applied some styles

--- src/components/FatherCounter.tsx
+++ src/components/FatherCounter.tsx
@@ -1,3 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    border: 1px solid blue;
+    min-height: 300px;
+    min-width: 600px;
+`;
 export const FatherCounter = () => {
-    return <div>Father Component</div>;
+    return <Container>Father Component</Container>;
 };
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -2,6 +2,8 @@ import { FatherCounter } from "./FatherCounter";
 import styled from "styled-components";
 
 const Container = styled.div`
+    display: flex;
+    flex-direction: row;
     border: 1px solid black;
     min-height: 300px;
     min-width: 600px;
@@ -9,10 +11,10 @@ const Container = styled.div`
 
 export const MainContainer = () => {
     return (
-        <>
+        <Container>
             <FatherCounter />
             <FatherCounter />
             <FatherCounter />
-        </>
+        </Container>
     );
 };
