2d8aa9048ecb1dd9e96de5ad24aa784b77cfafe1
ddfilipov <denisfilipov12+3@gmail.com>
2022-11-20 15:16:51 +0100

    okay better style now

--- src/components/FatherCounter.tsx
+++ src/components/FatherCounter.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Container = styled.div`
     border: 1px solid blue;
     min-height: 300px;
-    min-width: 600px;
+    min-width: 300px;
 `;
 export const FatherCounter = () => {
     return <Container>Father Component</Container>;
--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,20 +1,34 @@
 import { FatherCounter } from "./FatherCounter";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 100vh;
+`;
+
 const Container = styled.div`
     display: flex;
     flex-direction: row;
+    align-items: center;
+    gap: 10px;
+    justify-content: center;
+    padding: 10px;
+    height: 500px;
+    width: 1200px;
     border: 1px solid black;
-    min-height: 300px;
-    min-width: 600px;
 `;
 
 export const MainContainer = () => {
     return (
-        <Container>
-            <FatherCounter />
-            <FatherCounter />
-            <FatherCounter />
-        </Container>
+        <Wrapper>
+            <Container>
+                <FatherCounter />
+                <FatherCounter />
+                <FatherCounter />
+            </Container>
+        </Wrapper>
     );
 };
