7ad634495de50e8e0cb4f0c917bd4d12cedbf430
ddfilipov <denisfilipov12+3@gmail.com>
2022-11-20 14:38:03 +0100

    styled components finally working

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,5 +1,11 @@
 import { FatherCounter } from "./FatherCounter";
+import styled from "styled-components";
 
+const Container = styled.div`
+    border: 1px solid black;
+    min-height: 300px;
+    min-width: 600px;
+`;
 
 export const MainContainer = () => {
     return (
